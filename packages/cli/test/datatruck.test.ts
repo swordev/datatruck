b3bc124a49c7b31ce5c4bdd7567339908416dc07
Juanra GM <juanrgm724@gmail.com>
2022-07-07 12:53:49 +0200

    test: add more file changes

--- packages/cli/test/datatruck.test.ts
+++ packages/cli/test/datatruck.test.ts
@@ -114,7 +114,53 @@ describe("datatruck", () => {
   it.each(repositoryTypes)("backup, restore, prune %p", async (type) => {
     const fileChanges: FileChanges[] = [
       {
-        "file1.json": JSON.stringify({ id: 2 }),
+        file1: "contents",
+        // https://github.com/restic/restic/issues/3760
+        ...(type === "restic" && {
+          empty: "",
+        }),
+      },
+      {},
+      { file1: "contents2" },
+      { file1: false },
+      {
+        folder1: {},
+      },
+      {
+        folder1: {
+          "file1.json": JSON.stringify({ hello: "world" }),
+          folder2: {
+            folder3: {
+              folder4: {
+                folder5: {
+                  folder6: {
+                    ".file": "*",
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      {
+        folder1: {
+          folder2: {
+            folder3: {
+              ...Array.from({ length: 20 })
+                .fill(0)
+                .map((v, i) => `file_${i}`)
+                .reduce((result, name) => {
+                  result[name] = `filename: ${name}`;
+                  return result;
+                }, {} as Record<string, string>),
+            },
+          },
+        },
+      },
+      {
+        folder1: {
+          "file.bin": Buffer.from([1, 2, 3, 4]),
+        },
       },
     ];
 
@@ -141,10 +187,6 @@ describe("datatruck", () => {
         {}
       )
     ).toBe(0);
-
-    await fileChanger.update({
-      "file1.json": JSON.stringify({ id: 1 }),
-    });
 
     const backupResults: Awaited<ReturnType<typeof expectSuccessBackup>>[] = [];
 
