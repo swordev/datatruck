2e6549969f37e0aeefbc2fba1ee93c17b349e12c
Juanra GM <juanrgm724@gmail.com>
2023-09-29 14:43:10 +0200

    test(cli): keep temp dir in debug mode

--- packages/cli/test/globalSetup.ts
+++ packages/cli/test/globalSetup.ts
@@ -4,9 +4,10 @@ import { rm } from "fs/promises";
 export default async function () {
   return async () => {
     try {
-      await rm(parentTmpDir(), {
-        recursive: true,
-      });
+      if (!process.env.DEBUG)
+        await rm(parentTmpDir(), {
+          recursive: true,
+        });
     } catch (error) {
       if ((error as NodeJS.ErrnoException).code !== "ENOENT") throw error;
     }
