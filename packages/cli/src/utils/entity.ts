579196c1cb0bb6e2da8bbeb760ada23c221b4e71
Juanra GM <juanrgm724@gmail.com>
2023-10-09 09:45:32 +0200

    refactor: remove unused code

--- packages/cli/src/utils/entity.ts
+++
@@ -1,6 +0,0 @@
-export function makeTableSelector<T>(tableName: string) {
-  const q = (value: unknown) => `\`${value}\``;
-  const cb = (name?: keyof T) => `${q(tableName)}${name ? `.${q(name)}` : ""}`;
-  cb.toString = cb;
-  return cb;
-}
